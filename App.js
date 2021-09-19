367ee9bc2d1f1bef1f5219dbcbe9839c7cb5c391
hiepn4759 <hiepn4759@gmail.com>
2021-09-20 01:30:08 +0700

    update camera

--- App.js
+++ App.js
@@ -1,77 +1,60 @@
-import React, {useEffect, useState} from 'react';
-import {View, Text, TextInput, StyleSheet, Button} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import { RNCamera } from 'react-native-camera';
 import FreeOtp from 'react-native-freeotp';
 import totp from  "totp-generator";
+import {
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
 
-const App = () => {
+const App = () =>{
   const [code, setCode] = useState(0);
-  const [codeCs, setCodeCs] = useState(0);
   const [qr, setQr] = useState('');
-  const [qr1, setQr1] = useState('');
-  
+
+  onSuccess = async e => {
+    console.log(JSON.stringify(e));
+    const result = e.data;
+    setQr(result);
+    const tokenPair = await FreeOtp.getTokenPair(result);
+    setCode(JSON.stringify(tokenPair));
+  };
+
+  useEffect(() => {
+    const timeout = setTimeout( async () => {
+      if(qr == '') return;
+      const equToken = await FreeOtp.getTokenPair(qr);
+      if(equToken === code) return;
+      setCode(JSON.stringify(equToken));
+    }, 1000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [code]);
+
   return (
-    <View>
-      <Text style={{color: 'red'}}>SSO TOTP - HIPNS</Text>
-      <TextInput
-        style={styles.input}
-        onChangeText={(text) => {
-          setQr(text)
-        }}
-        value={qr}
+    <View style={{flex: 1}}>
+    <View  style={{flex: 4}}>
+      <QRCodeScanner
+        onRead={this.onSuccess}
+          flashMode={RNCamera.Constants.FlashMode.off}
       />
-      <TextInput
-        style={styles.input1}
-        onChangeText={(text) => {
-          setQr1(text)
-        }}
-        value={qr1}
-      />
-
-      <Button
-        title="totpFree"
-        onPress={
-          async () => {
-            console.log('totpFree');
-            const tokenPair = await FreeOtp.getTokenPair(qr);
-            console.log(JSON.stringify(tokenPair));
-            setCode(JSON.stringify(tokenPair));
-          }
-        }
-      />
-
-      <Button
-        title="totpCustom"
-        onPress={
-          async () => {
-            console.log('totpCustom');
-            const tokenCt = totp(qr1, { digits: 6 });
-            setCodeCs(tokenCt);
-          }
-        }
-      />
-      <Text>totpFree: {code}</Text>
-      <Text>totpFree: {qr}</Text>
-
-      <Text>totpCustom: {codeCs}</Text>
-      <Text>totpCustom: {qr1}</Text>
+      </View>
+      <View style={{flex: 2, justifyContent: 'center', alignItems: 'center'}}>
+        <Text style={{color: 'blue', fontSize: 14}}>Code:  {code}</Text>
+        <Text style={{color: 'blue', fontSize: 14}}>tokenOne:  {JSON.parse(code).tokenOne}</Text>
+        <Text style={{color: 'blue', fontSize: 14}}>tokenTwo:  {JSON.parse(code).tokenTwo}</Text>
+        <Text style={{color: 'blue', fontSize: 14}}>tokenOneExpires:  {JSON.parse(code).tokenOneExpires}</Text>
+        <Text style={{color: 'blue', fontSize: 14}}>tokenTwoExpires:  {JSON.parse(code).tokenTwoExpires}</Text>
+      </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-  },
-  input1: {
-    height: 40,
-    borderColor: 'red',
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-  },
+  
 });
 
 export default App;
